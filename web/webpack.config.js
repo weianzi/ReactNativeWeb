807b1a6f7e5d315c75b1f4d7a21454d5b15a3cc9
ancool <ancool@foxmail.com>
2016-02-18 15:26:40 +0800

    webpack参数说明

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -27,11 +27,7 @@ module.exports = {
   resolve: {
     alias: {
       'react-native': 'react-web',
-<<<<<<< HEAD
       //'ReactNativeART': 'react-art',
-=======
-      'ReactNativeART': 'react-art',
->>>>>>> 42f361b601d356bc0b1b4816a36c813640f23a9d
     },
     extensions: ['', '.js', '.jsx'],
   },
